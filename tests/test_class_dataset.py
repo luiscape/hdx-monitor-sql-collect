7cdfbe439578eee33548829b2e6b8475da7a73e2
Luis Capelo <luiscape@gmail.com>
2015-11-05 13:24:32 -0500

    adding proper dataset tests

---
+++ tests/test_class_dataset.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Unit tests for testing the Dataset class.
+
+'''
+import os
+import unittest
+
+from app.classes.dataset import Dataset
+
+class TestClassDataset:
+  '''
+  Performs tests on the Dataset calss.
+
+  '''
+  def __init__(self):
+    self.keys = [
+      'license_title', 'maintainer', 'package_creator',
+      'private', 'dataset_date', 'num_tags', 'id',
+      'metadata_created', 'indicator', 'methodology_other',
+      'caveats', 'metadata_modified', 'author', 'author_email',
+      'license_other', 'methodology', 'dataset_source',
+      'license_id', 'num_resources', 'creator_user_id',
+      'maintainer_email', 'name', 'notes', 'owner_org',
+      'title', 'revision_id'
+    ]
+
+  def test_dataset_keys(self):
+    '''
+    classes.dataset:  Test that the Dataset classe returns the right keys.
+
+    '''
+    result = Dataset('ebola-cases-2014').info()
+
+    for key in self.keys:
+      assert key in result.keys()
