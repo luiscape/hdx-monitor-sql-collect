b9b78559702bb2b2b319c7a6876b668d25e5515d
Luis Capelo <luiscape@gmail.com>
2015-11-05 18:24:05 -0500

    creating database tables

---
+++ app/database.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Configuring a
+
+'''
+import os
+import psycopg2
+
+from app.utilities.item import item
+from app.utilities.load import loadJSONFile
+
+HOST_DATABASE = os.environ.get('POSTGRES_PORT_5432_TCP_ADDR')
+
+def createTables(instance='config/dev.json', verbose=True):
+  '''
+  Creates tables in a PostgreSQL instance.
+
+  '''
+
+  #
+  # Loading database information
+  # from config file.
+  #
+  database = loadJSONFile(instance)['database']
+
+  #
+  # TODO: add environment variables
+  # to these default values.
+  #
+  conn = psycopg2.connect(host=HOST_DATABASE, dbname='metabase', user='metabase', password='metabase')
+  cur = conn.cursor()
+
+  #
+  # Build each table.
+  #
+  for table in database:
+
+    #
+    # Construct SQL statement.
+    #
+    table_sql = ""
+    for f in table['fields']:
+      s = '%s %s, ' % (f['field_name'], f['type'])
+      table_sql += s
+
+    statement = 'CREATE TABLE IF NOT EXISTS %s(%sPRIMARY KEY (%s))' % (table['name'], table_sql, ", ".join(table['primary_key']))
+
+    #
+    # Make statements to the database.
+    #
+    try:
+      cur.execute(statement)
+      conn.commit()
+      print("%s table `%s` created." % (item('prompt_bullet'), str(table['name'])))
+
+    except Exception as e:
+      print('%s Table `%s` could not be created.' % (item('prompt_error'), table['name']))
+      if verbose:
+        print(e)
+      return False
+
+  #
+  # Close communication.
+  #
+  cur.close()
+  conn.close()
+
