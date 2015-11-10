579d18f2c7b2dec39eb687eedad5431e43e9910a
Luis Capelo <luiscape@gmail.com>
2015-11-10 15:12:44 -0500

    loads worker blueprints

--- app/server.py
+++ app/server.py
@@ -7,14 +7,9 @@ script.
 
 '''
 import flask
-import app.utilities.load as Load
-
-from rq import Queue
-from redis import Redis
 
 from app.classes.ckan import CKAN
-from app.functions.fetch_class_data import fetchClassData
-
+from app.routes.queues import blueprint_queues
 from app.routes.status import blueprint_status
 from app.routes.datasets import blueprint_datasets
 from app.routes.countries import blueprint_countries
@@ -31,20 +26,10 @@ def createServer(database_uri, debug=False):
   app.host = '0.0.0.0'
 
   app.register_blueprint(blueprint_status)
+  app.register_blueprint(blueprint_queues)
   app.register_blueprint(blueprint_datasets)
   app.register_blueprint(blueprint_countries)
   app.register_blueprint(blueprint_revisions)
   app.register_blueprint(blueprint_gallery_items)
-
-  #
-  # Helper endpoints
-  #
-  @app.route('/all')
-  def computeAll():
-    '''
-    Computes information about all CKAN objects.
-
-    '''
-    print('foo')
 
   return app
