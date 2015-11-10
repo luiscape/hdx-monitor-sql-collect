8b9f0ff57bf1a2de796eb5311907a264d77ac335
Luis Capelo <luiscape@gmail.com>
2015-11-10 12:49:44 -0500

    merging routes script with server

--- app/server.py
+++ app/server.py
@@ -7,10 +7,19 @@ script.
 
 '''
 import flask
+import app.utilities.load as Load
 
-# from app.routes import defineApplicationRoutes
+from rq import Queue
+from redis import Redis
+
+from app.classes.ckan import CKAN
+from app.functions.fetch_class_data import fetchClassData
+
 from app.routes.status import blueprint_status
+from app.routes.datasets import blueprint_datasets
 from app.routes.countries import blueprint_countries
+from app.routes.revisions import blueprint_revisions
+from app.routes.gallery_items import blueprint_gallery_items
 
 def createServer(database_uri, debug=False):
   '''
@@ -22,8 +31,20 @@ def createServer(database_uri, debug=False):
   app.host = '0.0.0.0'
 
   app.register_blueprint(blueprint_status)
+  app.register_blueprint(blueprint_datasets)
   app.register_blueprint(blueprint_countries)
+  app.register_blueprint(blueprint_revisions)
+  app.register_blueprint(blueprint_gallery_items)
 
-  # defineApplicationRoutes(app)
+  #
+  # Helper endpoints
+  #
+  @app.route('/all')
+  def computeAll():
+    '''
+    Computes information about all CKAN objects.
+
+    '''
+    print('foo')
 
   return app
